4692ca168273655ff1bafcdc2b4e61b33a89055b
Finley <finleyrowles09@gmail.com>
2023-11-01 14:54:53 +0000

    Version 1 - Displaying a single char

---
+++ main.cpp
@@ -0,0 +1,86 @@
+#include "mbed.h"
+#include <string>
+
+// Initialise Pins D9 & D8 as Didital inputs for the two push buttons
+DigitalIn pb1(D9);
+DigitalIn pb2(D8);
+
+//  Initialise Pins D1 to D7 as Digital outputs for the segments of the Displaly, not including the DP
+DigitalOut segA(D1);
+DigitalOut segB(D0);
+DigitalOut segC(D4);
+DigitalOut segD(D5);
+DigitalOut segE(D6);
+DigitalOut segF(D2);
+DigitalOut segG(D3);
+
+class segmentDisplay
+{
+
+    // Private array storing the state of each segement requierd to display each letter of the alphabet
+    // column 0-7 as segments a-g
+    int letters[26][7] = 
+    {
+        {1,1,1,1,1,0,1}, //a
+        {0,0,1,1,1,1,1}, //b
+        {0,0,0,1,1,0,1}, //c
+        {0,1,1,1,1,0,1}, //d
+        {1,0,0,1,1,1,1}, //e
+        {1,0,0,0,1,1,1}, //f
+        {1,0,1,1,1,1,0}, //g
+        {0,0,1,0,1,1,1}, //h
+        {1,0,0,0,1,0,0}, //i
+        {1,0,1,1,0,0,0}, //j
+        {1,0,1,0,1,1,1}, //k
+        {0,0,0,1,1,1,0}, //l
+        {1,0,1,0,1,0,1}, //m
+        {0,0,1,0,1,0,1}, //n
+        {0,0,1,1,1,0,1}, //o
+        {1,1,0,0,1,1,1}, //p
+        {1,1,1,0,0,1,1}, //q
+        {0,0,0,0,1,0,1}, //r
+        {1,0,1,1,0,1,0}, //s
+        {0,0,0,1,1,1,1}, //t
+        {0,0,1,1,1,0,0}, //u
+        {0,1,0,1,0,1,0}, //v
+        {0,1,0,1,0,1,1}, //w
+        {0,0,1,0,1,0,0}, //x
+        {0,1,1,1,0,1,1}, //y
+        {1,1,0,1,1,0,0}, //z
+    };
+
+    public:
+
+        // function to display a single letter on the display
+        void displayChar(char charToDisplay)
+        {
+            //Convets the character to display into its zero indexed position in the alphabet
+            int charRow = (int)(charToDisplay % 32) - 1;
+
+            // Set each segment to the correct state as in the letters array
+            segA = (letters[charRow][0]);
+            segB = (letters[charRow][1]);
+            segC = (letters[charRow][2]);
+            segD = (letters[charRow][3]);
+            segE = (letters[charRow][4]);
+            segF = (letters[charRow][5]);
+            segG = (letters[charRow][6]);
+        }
+
+};
+
+
+// main() runs in its own thread in the OS
+int main()
+{   
+
+    //  Initialise an object sg of class SegmentDisplay
+    segmentDisplay sg;
+    // atempt to display a letter on the display
+    sg.displayChar('f');
+
+    // when the program has completed, do nothing forever
+    while (true) {
+        sleep();
+    }
+}
